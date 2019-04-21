d21d204abbc790e53160f9488b9cf010e1f39e94
Amit Sharma <amit.sharma.th@gmail.com>
2019-04-21 16:33:31 -0700

    updated palette

--- R/flip_xaxis_text.R
+++ R/flip_xaxis_text.R
@@ -1,7 +1,5 @@
 flip_xaxis_text<-function(){
   ggplot2::theme(
-    axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10),angle = 90),
-
+    axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10),angle = 90)
   )
-
 }
--- R/pallete1.R
+++ R/pallete1.R
@@ -2,7 +2,29 @@
 #' @return the list of rgb value strings
 #' examples
 #' pattete1()
-pallete1 <- function() {
-  c("#FAAB18", "#1380A1","#990000", "#588300")
+palettes <- list (
+  bbc = c("#FAAB18", "#1380A1","#990000", "#588300")
+)
 
+
+mo_palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  pal <- palettes[[name]]
+  if (is.null(pal))
+    stop("palette not available.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than palette unique colors")
+  }
+
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal)(n),
+                discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
 }
